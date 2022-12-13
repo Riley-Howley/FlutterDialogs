96e6d950618e38b89c9e247cb161851ec01fb572
Riley Howley <howleyriley@gmail.com>
2022-12-14 09:16:05 +1300

    Added Warning Choice dialog

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -160,6 +160,106 @@ class HomeScreen extends StatelessWidget {
           ),
         );
 
+    Future warningChoiceDialog() => showDialog(
+          context: context,
+          builder: (context) => Dialog(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(25),
+            ),
+            child: Container(
+              height: MediaQuery.of(context).size.height / 2,
+              child: Column(
+                children: [
+                  Container(
+                    decoration: BoxDecoration(
+                        color: Color.fromARGB(70, 244, 244, 244),
+                        borderRadius: BorderRadius.only(
+                          bottomLeft: Radius.circular(80),
+                          bottomRight: Radius.circular(80),
+                          topLeft: Radius.circular(25),
+                          topRight: Radius.circular(25),
+                        )),
+                    width: MediaQuery.of(context).size.width,
+                    height: MediaQuery.of(context).size.height / 5,
+                    child: Icon(
+                      Iconsax.warning_2,
+                      color: Colors.orange,
+                      size: 124,
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(28.0),
+                    child: Container(
+                      child: Text(
+                        "Success",
+                        style: TextStyle(
+                          fontSize: 24,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 10.0, right: 10.0),
+                    child: Center(
+                      child: Container(
+                        margin: EdgeInsets.only(bottom: 10),
+                        child: Text(
+                          "Warning! Message to go with the error",
+                          style: TextStyle(
+                            fontSize: 18,
+                            color: Colors.grey,
+                          ),
+                          textAlign: TextAlign.center,
+                        ),
+                      ),
+                    ),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Container(
+                        width: MediaQuery.of(context).size.width / 3,
+                        child: ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                              backgroundColor: Colors.green,
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(25),
+                              )),
+                          onPressed: () {
+                            Navigator.pop(context);
+                          },
+                          child: Text(
+                            "Continue",
+                            style: TextStyle(fontSize: 18),
+                          ),
+                        ),
+                      ),
+                      Container(
+                        width: MediaQuery.of(context).size.width / 3,
+                        child: ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                              backgroundColor: Colors.orange,
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(25),
+                              )),
+                          onPressed: () {
+                            Navigator.pop(context);
+                          },
+                          child: Text(
+                            "Cancel",
+                            style: TextStyle(fontSize: 18),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ),
+        );
+
     Future errorDialog() => showDialog(
           context: context,
           builder: (context) => Dialog(
@@ -257,6 +357,12 @@ class HomeScreen extends StatelessWidget {
                 warningDialog();
               },
               child: Text("Warning"),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                warningChoiceDialog();
+              },
+              child: Text("Warning Choices"),
             ),
           ],
         ),
